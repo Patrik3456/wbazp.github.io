3c03cc1b417dba47ba73df35f7eff78c0581887b
Patrik <114060986+Patrik3456@users.noreply.github.com>
2024-05-12 08:05:58 +0200

    Delete JavaScript directory

--- JavaScript/Header_scroll.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scroll()};
-
-function scroll() {
-    if (document.documentElement.scrollTop > 20 ) {
-        document.getElementById("nav-bar").className = "scroll";
-    } else {
-        document.getElementById("nav-bar").className = "";
-    }
-}
